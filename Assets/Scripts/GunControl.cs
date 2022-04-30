5a1e5d6a8f0198a88a2ab5f8c5f67acbd73f635a
mari <63924379+Cookieglue@users.noreply.github.com>
2022-04-30 19:38:19 -0400

    Basic Gun Script

---
+++ Assets/Scripts/GunControl.cs
@@ -0,0 +1,34 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GunControl : MonoBehaviour
+{
+    private AudioSource audioSource;
+
+    void Start(){
+
+        audioSource = GetComponent<AudioSource>();
+
+    }
+    void Update()
+    {
+        //shoot
+        if (Input.GetMouseButtonDown(0)){
+
+            audioSource.Play();
+
+            RaycastHit hit;
+            if (Physics.Raycast(transform.position, transform.forward, out hit)){
+
+                if (hit.transform.gameObject.tag == "enemy") {
+
+                    //Deal Damage to enemy but we need enemies first
+
+                }
+
+            }
+
+        }
+    }
+}
