357e94c7591e5f4574e0598c6b10eed5926e6bbf
mari <63924379+Cookieglue@users.noreply.github.com>
2022-05-01 12:31:12 -0400

    Primitive AI

--- Assets/Scripts/AI Related/BasicEnemy.cs
+++ Assets/Scripts/AI Related/BasicEnemy.cs
@@ -1,18 +1,54 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class BasicEnemy : MonoBehaviour
 {
-    // Start is called before the first frame update
+    
+    [SerializeField] private float health;
+    [SerializeField] private LayerMask layerMask;
+    [SerializeField] private float speed;
+    [SerializeField] private float agrowRange = 10;
+    [SerializeField] private float attackRange = 1;
+    private NavMeshAgent navMeshAgent;
+    private Transform player;
+    private Transform baseCore;
+    private RaycastHit hit;
     void Start()
     {
-        
+        navMeshAgent = GetComponent<NavMeshAgent>();
+        player = GameObject.FindWithTag("Player").GetComponent<Transform>();
+        baseCore = GameObject.FindWithTag("baseCore").GetComponent<Transform>();
+        navMeshAgent.speed = speed;
     }
-
-    // Update is called once per frame
     void Update()
     {
-        
+        float playerDist = Vector3.Distance(player.position, transform.position);
+        float baseCoreDist = Vector3.Distance(baseCore.position, transform.position);
+
+        //Set Navigation
+        if (playerDist <= agrowRange || playerDist < baseCoreDist){
+
+            navMeshAgent.SetDestination(player.position);
+
+        }
+        else {
+
+            navMeshAgent.SetDestination(baseCore.position);
+
+        }
+
+        //Attack Regardless of whatis there
+        if (playerDist < attackRange || baseCoreDist < attackRange){
+
+            if (Physics.BoxCast(transform.position, transform.localScale , transform.forward, out hit, transform.rotation,  attackRange, layerMask)){
+
+                //hit.transform.gameObject.
+
+            }
+
+        }
+
     }
 }
