f91097bfe30080d493e4dc3dd80fd30ef5eb9d09
mari <63924379+Cookieglue@users.noreply.github.com>
2022-05-02 18:27:39 -0400

    Fun Shooting

--- Assets/Scripts/Player Related/GunControl.cs
+++ Assets/Scripts/Player Related/GunControl.cs
@@ -4,22 +4,32 @@ using UnityEngine;
 
 public class GunControl : MonoBehaviour
 {
+    private Transform head;
     private AudioSource audioSource;
+    private ParticleSystem particleSource;
+    private Animator animator;
 
     void Start(){
 
         audioSource = GetComponent<AudioSource>();
+        particleSource = gameObject.transform.GetChild(0).gameObject.GetComponent<ParticleSystem>();
+        head = GetComponentInParent<Transform>();
+        animator = GetComponent<Animator>();
 
     }
     void Update()
     {
+        Debug.DrawRay(head.position, transform.forward);
         //shoot
         if (Input.GetMouseButtonDown(0)){
 
             audioSource.Play();
+            particleSource.Clear();
+            particleSource.Play();
+            animator.Play("PistolShoot");
 
             RaycastHit hit;
-            if (Physics.Raycast(transform.position, transform.forward, out hit)){
+            if (Physics.Raycast(head.position, transform.right, out hit)){
 
                 if (hit.transform.gameObject.tag == "enemy") {
 
